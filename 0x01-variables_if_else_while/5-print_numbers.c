0099d27e644949795c601b0deb32e27330e8bfd2
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-22 16:39:09 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
 /**
- * main - program that prints the alphabet in lowercase and upper case
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *                followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
-	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
-	}
-
-	putchar('\n');
-
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0); 
 }
-

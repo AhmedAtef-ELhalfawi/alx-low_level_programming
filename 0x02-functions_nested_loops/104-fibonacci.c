bc01b54b2a36a13f08819e6fa061435a86de2919
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 15:54:06 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,39 +7,24 @@
  */
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 1, fib2 = 2;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+    int count;
+    unsigned long fib1 = 1, fib2 = 2, next_fib;
 
-	printf("%lu, %lu, ", fib1, fib2);
-	for (count = 2; count < 98; count++)
-	{
-		/* Calculate next Fibonacci number using half-values */
-		fib1_half1 = fib1 / 10000000000;
-		fib1_half2 = fib1 % 10000000000;
-		fib2_half1 = fib2 / 10000000000;
-		fib2_half2 = fib2 % 10000000000;
+    printf("%lu, %lu, ", fib1, fib2);
+    for (count = 2; count < 98; count++)
+    {
+        next_fib = fib1 + fib2;
+        printf("%lu", next_fib);
 
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 + fib2_half2;
+        if (count != 97) 
+        {
+            printf(", ");
+        }
 
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-
-		fib1 = fib1_half1 * 10000000000 + fib1_half2;
-		fib2 = fib2_half1 * 10000000000 + fib2_half2;
-
-		printf("%lu", fib2);
-		if (count != 97) 
-		{
-			printf(", ");
-		}
-	}
-	printf("\n");
-	return (0);
+        fib1 = fib2;
+        fib2 = next_fib;
+    }
+    printf("\n");
+    return 0;
 }
 

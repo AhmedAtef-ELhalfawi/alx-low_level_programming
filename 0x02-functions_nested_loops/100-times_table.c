85be3a51f4533022c24d4c83ad0665d420a64a42
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 16:00:45 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,43 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
+ * print_times_table - Prints the times table for a given number.
+ * @n: The number to calculate the times table for.
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+    int i, j, result;
 
-	if (!(n > 15 || n < 0))
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
-				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 100 && j != 0)
-				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
-				}
-				else
-					_putchar((res % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+    if (n >= 0 && n <= 15) // Input validation (clearer condition)
+    {
+        for (i = 0; i <= n; i++)
+        {
+            for (j = 0; j <= n; j++)
+            {
+                result = i * j;
+
+                // Print the result with proper formatting
+                if (j != 0) 
+                {
+                    _putchar(','); // Separator for non-initial entries
+                    _putchar(' ');  // Space for alignment
+                }
+
+                // Handle leading spaces and multi-digit numbers
+                if (result < 10) 
+                    _putchar(' ');
+                if (result < 100)
+                    _putchar(' ');
+                
+                // Print the digits of the result
+                if (result >= 100) 
+                    _putchar((result / 100) + '0');
+                if (result >= 10)
+                    _putchar(((result / 10) % 10) + '0');
+                _putchar((result % 10) + '0'); 
+            }
+            _putchar('\n'); // Move to the next line for a new row
+        }
+    }
 }
+

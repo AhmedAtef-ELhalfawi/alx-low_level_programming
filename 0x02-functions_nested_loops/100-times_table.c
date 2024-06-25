468b34d4a6bb085246e4752d58471ffe4f90597f
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 15:15:22 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,33 +1,34 @@
 #include "main.h"
 
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: the number of rows and columns in the times table
+ */
 void print_times_table(int n) {
-    int i, j, res;
+    if (n < 0 || n > 15)
+        return;  // Exit if n is out of range
 
-    if (!(n > 15 || n < 0)) {
-        for (i = 0; i <= n; i++) {
-            for (j = 0; j <= n; j++) {
-                res = (i * j);
-                if (j != 0) {
-                    putchar(',');
-                    putchar(' ');
-                }
-                if (res < 10 && j != 0) {
-                    putchar(' ');
-                    putchar(' ');
-                    putchar((res % 10) + '0');
-                } else if (res >= 10 && res < 100) {
-                    putchar(' ');
-                    putchar((res / 10) + '0');
-                    putchar((res % 10) + '0');
-                } else if (res >= 100 && j != 0) {
-                    putchar((res / 100) + '0');
-                    putchar((res / 10) % 10 + '0');
-                    putchar((res % 10) + '0');
-                } else {
-                    putchar((res % 10) + '0');
-                }
+    int i, j, product;
+
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= n; j++) {
+            product = i * j;
+
+            // Print commas and spaces except before the first column
+            if (j > 0) {
+                printf(", ");
             }
-            putchar('\n');
+
+            // Adjust spacing based on the value of product
+            if (product < 10) {
+                printf("  %d", product);
+            } else if (product < 100) {
+                printf(" %d", product);
+            } else {
+                printf("%d", product);
+            }
         }
+        printf("\n");
     }
 }
+

2d8720b70c0f7a3f40dab7d0693aa63092e341da
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 15:34:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h> // Add this line
-
 /**
  * print_times_table - prints the times table for n.
  * @n: The multiplication table requested.
@@ -8,34 +6,42 @@
  */
 void print_times_table(int n)
 {
-    int i, j, result; // Move variable declarations here
+	int i, j, res;
 
-    if (n < 0 || n > 15)
-    {
-        /* Exit if n is out of range */
-        return;
-    }
-
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            result = (i * j);
-            if (j != 0)
-            {
-                printf(", ");
-            }
-            if (result < 10 && j != 0)
-            {
-                printf("   ");
-            }
-            else if (result >= 10 && result < 100)
-            {
-                printf("  ");
-            }
-            printf("%d", result);
-        }
-        printf("\n");
-    }
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
-

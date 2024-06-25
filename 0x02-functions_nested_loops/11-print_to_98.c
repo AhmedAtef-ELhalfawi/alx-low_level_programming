e25e9ad316633f40ff0d1dc2f54ea11740b4aece
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 14:22:49 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,31 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * print_to_98 - print n to 98 counts
- *           separated by comma, followed
- *           by space and number should be
- *           printed in order
+ * print_to_98 - print numbers from n to 98
+ *               separated by commas followed
+ *               by a space, in order
  *
- * @n: input
-*/
-
+ * @n: the starting number
+ */
 void print_to_98(int n)
 {
 	int counter;
 
 	if (n > 98)
+	{
 		for (counter = n; counter > 98; --counter)
+		{
 			printf("%d, ", counter);
+		}
+	}
 	else
+	{
 		for (counter = n; counter < 98; ++counter)
+		{
 			printf("%d, ", counter);
+		}
+	}
 	printf("98\n");
 }
+

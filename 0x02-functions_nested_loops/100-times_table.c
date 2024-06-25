eb8ac2a55a7ed4ff1977f3c3298d803496959ad8
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 16:41:40 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,18 +1,49 @@
-#include <stdio.h>
+#include "main.h"
 
-void print_times_table(int n) {
-    if (n < 0 || n > 15) {
+/**
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: Nothing.
+ */
+void print_times_table(int n)
+{
+    int i, j, res;
+
+    if (n < 0 || n > 15)
         return;
-    }
 
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
-            if (j == 0) {
-                printf("%d", i * j);
-            } else {
-                printf(", %3d", i * j);
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            res = i * j;
+
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (res < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else if (res < 100)
+                {
+                    _putchar(' ');
+                }
             }
-        }
-        printf("\n");
-    }
-}
+
+            if (res < 10)
+            {
+                _putchar(res + '0');
+            }
+            else if (res < 100)
+            {
+                _putchar((res / 10) + '0');
+                _putchar((res % 10) + '0');
+            }
+            else
+            {
+                _putchar((res / 100) + '0
+

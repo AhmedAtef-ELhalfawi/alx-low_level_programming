068c069555bd56a8b396debd78a40751e94591b0
AhmedAtef-ELhalfawi <ahmedatef221b@gmail.com>
2024-06-25 17:13:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,56 +1,61 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers, starting with
- *        1 and 2, separated by a comma followed by a space.
+ * print_fibonacci_numbers - Prints the Fibonacci numbers up to a limit.
+ * @limit: The number of Fibonacci numbers to print.
+ */
+void print_fibonacci_numbers(int limit)
+{
+	int count;
+	unsigned long fib1 = 1, fib2 = 2, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
+
+	/* Print the first two Fibonacci numbers */
+	printf("%lu, %lu", fib1, fib2);
+
+	/* Print the subsequent Fibonacci numbers */
+	for (count = 3; count <= limit; count++)
+	{
+		sum = fib1 + fib2;
+
+		/* Split the numbers if they exceed the limit for unsigned long */
+		if (count > 92)
+		{
+			fib1_half1 = fib1 / 1000000000;
+			fib2_half1 = fib2 / 1000000000;
+			fib1_half2 = fib1 % 1000000000;
+			fib2_half2 = fib2 % 1000000000;
+
+			half1 = fib1_half1 + fib2_half1;
+			half2 = fib1_half2 + fib2_half2;
+
+			if (half2 >= 1000000000)
+			{
+				half1 += 1;
+				half2 %= 1000000000;
+			}
+
+			printf(", %lu%09lu", half1, half2);
+		}
+		else
+		{
+			printf(", %lu", sum);
+		}
+
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point of the program.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    int count;
-    unsigned long fib1 = 1, fib2 = 2, sum;
-    unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-    unsigned long half1, half2;
-
-    // Print the first 92 Fibonacci numbers
-    for (count = 3; count <= 92; count++)
-    {
-        sum = fib1 + fib2;
-        printf("%lu, ", sum);
-        fib1 = fib2;
-        fib2 = sum;
-    }
-
-    // Split the large Fibonacci numbers into halves
-    fib1_half1 = fib1 / 1000000000;
-    fib2_half1 = fib2 / 1000000000;
-    fib1_half2 = fib1 % 1000000000;
-    fib2_half2 = fib2 % 1000000000;
-
-    // Print the remaining Fibonacci numbers
-    for (count = 93; count < 99; count++)
-    {
-        half1 = fib1_half1 + fib2_half1;
-        half2 = fib1_half2 + fib2_half2;
-
-        if (fib1_half2 + fib2_half2 > 999999999)
-        {
-            half1 += 1;
-            half2 %= 1000000000;
-        }
-
-        printf("%lu%09lu", half1, half2);  // Use %09lu to ensure proper formatting
-        if (count != 98)
-            printf(", ");
-
-        fib1_half1 = fib2_half1;
-        fib1_half2 = fib2_half2;
-        fib2_half1 = half1;
-        fib2_half2 = half2;
-    }
-    printf("\n");
-    return (0);
+	print_fibonacci_numbers(98);
+	return (0);
 }
-
